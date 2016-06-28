caeebfbaa87d75fc8ed70f9b1b3582b822c0d12b
James Dabbs <jamesdabbs@gmail.com>
2016-06-28 18:19:04 -0400

    Working poll posting and responding
    Still need to figure out a solution for exporting that
    
    * is workably convenient for admins
    * doesn't let non-admins post polls or see polls that they didn't post

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,8 @@ Vagrant.configure("2") do |config|
   # config.vm.network "forwarded_port", guest: 80, host: 8080
   # config.vm.network "private_network", ip: "192.168.33.10"
   # config.vm.network "public_network"
-  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.synced_folder "/data/src/hedis-namespace", "/data/src/hedis-namespace"
+  config.vm.synced_folder "/data/src/replicant", "/data/src/replicant"
 
   config.vm.provider "virtualbox" do |vb|
   #   vb.gui = true
--- src/Plugins/Panic.hs
+++ src/Plugins/Panic.hs
@@ -7,8 +7,15 @@ import Base
 import Replicant
 import Replicant.Plugins.Base
 
+import           Data.Aeson
 import           Data.Attoparsec.Text
-import qualified Data.Text as T
+import qualified Data.ByteString.Lazy     as LBS
+import qualified Data.Map                 as M
+import           Data.Maybe               (fromJust)
+import qualified Data.Text                as T
+import           Data.Time.Clock          (UTCTime, getCurrentTime)
+import qualified Data.UUID                as UUID
+import qualified Data.UUID.V4             as UUID
 import qualified Database.Redis.Namespace as R
 
 panic :: Replicant e m => Plugin m
@@ -18,13 +25,15 @@ check :: Replicant e m => Handler m
 check = mkHandler "Start a panic score poll" True checkP [] $ \mroom -> do
   reply "I don't know, but I'll ask them"
   getRoom mroom >>= \case
-    Just roomId -> startPoll roomId
+    Just roomId -> do
+      user <- getSender
+      createPoll user roomId >>= deliverPoll
     Nothing -> reply "Sorry ... I couldn't figure out what room you meant"
 
 checkP :: Parser (Maybe Text)
 checkP = do
-  string "how"
-  manyTill anyChar (string "every" *> word)
+  _ <- string "how"
+  _ <- manyTill anyChar (string "every" *> word)
   whitespace
   optional (string "in " *> word)
 
@@ -32,8 +41,11 @@ checkP = do
 record :: Replicant e m => Handler m
 record = mkHandler "Record reported panic scores" False recordP [] $ \n -> do
   user <- getSender
-  respondToPoll user n
-  sendToUser user $ tshow n <> ", got it"
+  activePollFor user >>= \case
+    Nothing -> return () -- Should we indicate that there isn't an active poll?
+    Just poll -> do
+      respondToPoll poll user n
+      sendToUser user $ tshow n <> ", got it"
 
 recordP :: Parser Int
 recordP = do
@@ -51,24 +63,69 @@ getRoom mname = case mname of
   Just name -> getRoomByName name
   _         -> Just . messageRoom <$> getMessage
 
-startPoll :: Replicant e m => Room -> H m ()
-startPoll source = do
-  members <- getRoomMembers source
-  forM_ members $ \u -> sendToUser u "Hey, how are you doing today (on a scale of 1-6)?"
+deliverPoll :: Replicant e m => Poll -> H m ()
+deliverPoll Poll{..} = forM_ pollMemberIds $ \u -> sendToUserId u "Hey, how are you doing today (on a scale of 1-6)?"
 
-respondToPoll :: Replicant e m => User -> Int -> H m ()
-respondToPoll user n = do
-  poll <- activePollFor user
+respondToPoll :: Replicant e m => Poll -> User -> Int -> H m ()
+respondToPoll poll user n = do
   when (n > 4) $
-    sendToUser (pollPoster poll) $ "FYI, " <> tshow user <> " is at a " <> (T.pack $ show n)
+    sendToUserId (pollPosterId poll) $ "FYI, @" <> userName user <> " is at a " <> (T.pack $ show n)
   recordPollResponse poll user n
 
 data Poll = Poll
-  { pollPoster :: User
+  { pollId        :: UUID.UUID
+  , pollPosterId  :: UserId
+  , pollMemberIds :: [UserId]
+  , pollCreatedAt :: UTCTime
   }
 
-activePollFor :: User -> m Poll
-activePollFor = error "activePollFor"
+instance ToJSON Poll where
+  toJSON Poll{..} = object
+    [ "id"         .= UUID.toText pollId
+    , "poster_id"  .= pollPosterId
+    , "member_ids" .= pollMemberIds
+    , "created_at" .= pollCreatedAt
+    ]
 
-recordPollResponse :: Poll -> User -> Int -> m ()
-recordPollResponse = error "recordPollResponse"
+instance FromJSON Poll where
+  parseJSON = withObject "poll" $ \v -> do
+    pollId        <- fromJust . UUID.fromText <$> v .: "id" -- should we be concerned here?
+    pollPosterId  <- v .: "poster_id"
+    pollMemberIds <- v .: "member_ids"
+    pollCreatedAt <- v .: "created_at"
+    return Poll{..}
+
+data PollResults = PollResults
+  { prPoll      :: Poll
+  , prResponses :: M.Map UserId Int
+  }
+
+createPoll :: Replicant e m => User -> Room -> H m Poll
+createPoll user source = do
+  pollId  <- liftIO UUID.nextRandom
+  members <- map userId <$> getRoomMembers source
+  now     <- liftIO getCurrentTime
+  let poll = Poll pollId (userId user) members now
+
+  redis $ do
+    R.set ("polls:" <> UUID.toASCIIBytes pollId) (LBS.toStrict $ encode poll)
+    R.hmset "active-poll" $ map (\m -> (encodeUtf8 m, UUID.toASCIIBytes pollId)) members
+
+  return poll
+
+activePollFor :: Replicant e m => User -> H m (Maybe Poll)
+activePollFor User{..} = do
+  mpollId <- redis $ R.hget "active-poll" (encodeUtf8 userId)
+  case mpollId of
+    Nothing -> return Nothing
+    Just pollId -> do
+      poll <- redis $ R.get ("polls:" <> pollId)
+      case decode . LBS.fromStrict <$> poll of
+        Nothing -> return Nothing
+        Just p  -> return p
+
+
+recordPollResponse :: Replicant e m => Poll -> User -> Int -> H m ()
+recordPollResponse Poll{..} User{..} score = void . redis $ do
+  R.hdel "active-poll" [encodeUtf8 userId]
+  R.hset ("polls:" <> UUID.toASCIIBytes pollId <> ":responses") (encodeUtf8 userId) (encodeUtf8 $ tshow score)
